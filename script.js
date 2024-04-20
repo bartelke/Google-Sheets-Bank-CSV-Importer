97c2126dd824250e007a2f2c928603d689592363
Bartosz Kloc <atlf75800@gmail.com>
2024-04-20 21:47:54 +0200

    user authorization

---
+++ script.js
@@ -0,0 +1,124 @@
+// Assuming CLIENT_ID and API_KEY are injected through a secure process during development or build time.
+const CLIENT_ID = CLIENT_ID_ENV;
+const API_KEY = API_KEY_ENV;
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC =
+  "https://sheets.googleapis.com/$discovery/rest?version=v4";
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById("authorize_button").style.visibility = "hidden";
+document.getElementById("signout_button").style.visibility = "hidden";
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load("client", initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Initializes the API client with the discovery doc.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: "", // Callback defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById("authorize_button").style.visibility = "visible";
+  }
+}
+
+/**
+ * Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+    await listMajors();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+/**
+ * Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken("");
+    document.getElementById("content").innerText = "";
+    document.getElementById("authorize_button").innerText = "Authorize";
+    document.getElementById("signout_button").style.visibility = "hidden";
+  }
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet.
+ */
+async function listMajors() {
+  let response;
+  try {
+    // Fetch values from a spreadsheet
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
+      range: "Class Data!A2:E",
+    });
+  } catch (err) {
+    document.getElementById("content").innerText = err.message;
+    return;
+  }
+  const range = response.result;
+  if (!range || !range.values || range.values.length == 0) {
+    document.getElementById("content").innerText = "No values found.";
+    return;
+  }
+  // Flatten to string to display
+  const output = range.values.reduce(
+    (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+    "Name, Major:\n"
+  );
+  document.getElementById("content").innerText = output;
+}
